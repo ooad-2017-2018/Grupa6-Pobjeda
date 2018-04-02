505f2d719f5758679ade9f59141b27d7735d0475
Amar Lučkin <aluckin3@etf.unsa.ba>
2018-04-02 20:01:56 +0200

    Ispravke rez i nazivi neki

--- Projekat/e-market/e-market/Korisnik/FinalizacijaSpiska.xaml.cs
+++ Projekat/e-market/e-market/Korisnik/FinalizacijaSpiska.xaml.cs
@@ -20,9 +20,9 @@ namespace e_market
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class DeaktivirajNalog : Page
+    public sealed partial class FinalizacijaSpiska : Page
     {
-        public DeaktivirajNalog()
+        public FinalizacijaSpiska()
         {
             this.InitializeComponent();
         }
