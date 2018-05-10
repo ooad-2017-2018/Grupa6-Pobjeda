96ca8a9b88644930242482d96ce40eb05b445361
Amar Lučkin <aluckin3@etf.unsa.ba>
2018-05-10 12:17:15 +0200

    Rad na UWP-u

--- Projekat/e-market/e-market/App.xaml.cs
+++ Projekat/e-market/e-market/App.xaml.cs
@@ -14,6 +14,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using e_market.View;
+using e_market.ViewModel;
 
 namespace e_market
 {
@@ -66,7 +68,7 @@ namespace e_market
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                    rootFrame.Navigate(typeof(Prijava), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
